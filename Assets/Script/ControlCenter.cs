fa7c0ab77a6b4347ad3dba8efb0b51d32619cb1e
ShenWu <sunday781019@gmail.com>
2016-08-20 23:54:33 +0800

    first commit

---
+++ Assets/Script/ControlCenter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControlCenter : MonoBehaviour {
+
+	public Text _describe;
+	public Text[] _options = new Text[4];
+	public int questionNum;
+	public string _ans;
+	// Use this for initialization
+	void Start () {
+		_options = GameObject.Find("Options").GetComponentsInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Btn_Start(){
+		LoadQuestion(questionNum);
+	}
+	void LoadQuestion(int num){
+		if(LocalJson.qDatas == null){
+			//load Json
+			LocalJson.AddressToClass("question");
+			Debug.Log("Load qData");
+		}
+		if(num <= LocalJson.qDatas.Length && num > 0){
+			
+			LocalJson.QuestionData qData = LocalJson.qDatas[num - 1];
+			//load describe
+			_describe.text = qData.describe;
+			//load option
+			_options[0].text = qData.option_A;
+			_options[1].text = qData.option_B;
+			_options[2].text = qData.option_C;
+			_options[3].text = qData.option_D;
+			//load ans
+			_ans = qData.ans;
+		}else{
+			Debug.Log("num is out of range");
+		}
+		
+
+	}
+}
---
+++ Assets/Script/LocalJson.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using JsonFx.Json;
+
+public class LocalJson : MonoBehaviour {
+
+	public static TextAsset _txtJson;
+	public static QuestionData[] qDatas;
+	public class QuestionData{
+		public string id;
+		public string describe;
+		public string option_A;
+		public string option_B;
+		public string option_C;
+		public string option_D;
+		public string ans;
+	}
+
+	public static void AddressToClass(string address){
+		_txtJson = Resources.Load(address) as TextAsset;
+		qDatas = JsonReader.Deserialize<QuestionData[]>(_txtJson.text);
+	}
+	
+}
